2693b09fdadda14846db9053182722a68958353e
Sinisa <sinisha.stojanovic@gmail.com>
2026-07-19 10:40:37 +0200

    Argument validation and string argument parsing to integer array

--- rush_01/old/rush01.c
+++ rush_01/old/rush01.c
@@ -5,31 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sstojano <sstojano@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/18 13:42:11 by sstojano          #+#    #+#             */
-/*   Updated: 2026/07/19 10:33:05 by sstojano         ###   ########.fr       */
+/*   Created: 2026/07/18 14:15:54 by sstojano          #+#    #+#             */
+/*   Updated: 2026/07/19 00:55:38 by sstojano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	arg_validation(int ac, char **av);
-void	parse_arg_str_to_int_arr(char *arg_input, int *arg_nbrs);
+int	*arg_validation(int ac, int *arg_nbrs, char *av);
 
 int	main(int argc, char **argv)
 {
 	int	arg_nbrs[16];
 	int	i;
 
-	// argument validation check
-	if (arg_validation(argc, argv) == 0)
-		return (0);
-	// parsing string argument to int array
-	parse_arg_str_to_int_arr(argv[1], arg_nbrs);
+	arg_validation(argc, arg_nbrs, argv[1]);
 	i = 0;
 	printf("main() function print starts:\n");
 	while (i < 16)
 	{
-		printf("%d ", arg_nbrs[i]);
+		printf("%d, ", arg_nbrs[i]);
 		i++;
 	}
 	printf("\n");
