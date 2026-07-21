ec689ce10b6362cbe2f6de1529a742a98cee1a9a
sstojano <sstojano@student.42belgium.be>
2026-07-21 18:26:33 +0200

    ex12 calculating hax value for memory address

--- 04_c_piscine_c_02_single_point_of_truth/ex12/ft_print_memory.c
+++ 04_c_piscine_c_02_single_point_of_truth/ex12/ft_print_memory.c
@@ -14,12 +14,34 @@
 
 void *ft_print_memory(void *addr, unsigned int size)
 {
+	int	i;
 	unsigned long address;
+	char	hex_address[17];
 	// get address number
 	address = (unsigned long) addr;
 	if (size > 0)	
-		printf("%ld", address);
-	//convert address number to hex 16 chars long
+		printf("%ld\n\n\n", address);
+	//convert address number to hex 16 chars long and fill with zeros from start
+	printf("Int address: %ld\n", address);
+	i = 0;
+	while (address > 0)
+	{
+		int remain;
+		
+		remain = address % 16;
+		if(remain < 10)
+			hex_address[i] = '0' + remain;
+		else
+			hex_address[i] = ('a' + remain - 10);
+		address /= 16;
+		i++;
+	}
+	printf("Int address: %ld\n", address);
+	printf("Hex address: ");
+	while (i--)
+	{
+		printf("%c", hex_address[i]);
+	}
 	
 	
 	//print ": "
