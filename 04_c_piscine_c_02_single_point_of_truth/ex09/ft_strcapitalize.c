5a1744251b7173fd56f84a72671d6e41be5c64f0
Sinisa <sinisha.stojanovic@gmail.com>
2026-07-21 10:39:55 +0200

    ex07, ex08 and ex09 finished

--- 04_c_piscine_c_02_single_point_of_truth/ex09/ft_strcapitalize.c
+++ 04_c_piscine_c_02_single_point_of_truth/ex09/ft_strcapitalize.c
@@ -10,7 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strcapitalize(char *str)
+void	is_not_letter(char c, int *cap_flag)
 {
-	
+	if (((32 <= c && c <= 47) || (58 <= c && c <= 64)
+			|| (91 <= c && c <= 96)
+			|| (123 <= c && c <= 126)))
+		*cap_flag = 1;
+	if (48 <= c && c <= 57)
+		*cap_flag = 0;
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	capitalize_flag;
+
+	capitalize_flag = 1;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		is_not_letter(str[i], &capitalize_flag);
+		if (('a' <= str[i] && str[i] <= 'z')
+			|| ('A' <= str[i] && str[i] <= 'Z'))
+		{
+			if (capitalize_flag == 1 && ('A' <= str[i] && str[i] <= 'Z'))
+				capitalize_flag = 0;
+			else if (capitalize_flag == 0 && ('A' <= str[i] && str[i] <= 'Z'))
+				str[i] += 32;
+			else if (capitalize_flag == 1 && ('a' <= str[i] && str[i] <= 'z'))
+			{
+				str[i] -= 32;
+				capitalize_flag = 0;
+			}
+		}
+		i++;
+	}
+	return (str);
 }
--- 04_c_piscine_c_02_single_point_of_truth/ex09/main_test.c
+++ 04_c_piscine_c_02_single_point_of_truth/ex09/main_test.c
@@ -1,11 +1,24 @@
-char *ft_strcapitalize(char *str);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_test.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sstojano <sstojano@student.42belgium.be>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/20 23:35:54 by sstojano          #+#    #+#             */
+/*   Updated: 2026/07/20 23:36:01 by sstojano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcapitalize(char *str);
 
 int	main(void)
 {
-	char src_str[30];
-	char dest_str[30];
-
-	// src_str = "This is test string";
-
-	ft_strcpy(dest_str, src_str);
+	char	src_str[32] = "ABC dEf; XYZ-mnO 123abc DEF+gHi";
+	printf("Before capitalization:\n%s\n", src_str);
+	ft_strcapitalize(src_str);
+	printf("After capitalization:\n%s\n", src_str);
+	return (0);
 }
