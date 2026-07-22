fd10dbe259a0704e83521d0751b47dabee7a9fca
Sinisa <sinisha.stojanovic@gmail.com>
2026-07-22 21:59:50 +0200

    ex04 finished

--- 05_c_03_home/ex04/ft_strstr.c
+++ 05_c_03_home/ex04/ft_strstr.c
@@ -1,16 +1,29 @@
-char *ft_strstr(char *str, char *to_find)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sstojano <sstojano@student.42belgium.be>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/22 19:41:06 by sstojano          #+#    #+#             */
+/*   Updated: 2026/07/22 19:41:31 by sstojano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	j = 0;
-
 	while (str[i] != '\0')
 	{
-		
+		j = 0;
+		while (to_find[j] != '\0' && str[i + j] == to_find[j])
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
 		i++;
 	}
-	
-	return (str);
+	return ((char *) 0);
 }
--- 05_c_03_home/ex04/main.c
+++ 05_c_03_home/ex04/main.c
@@ -1,14 +1,21 @@
+#include <stdio.h>
+
 char *ft_strstr(char *str, char *to_find);
 
 int	main(void)
 {
 	char	*s1;
-	char	*s2;
+	// char	*s2;
 	char	*to_find;
+	char	*result;
 
-	s1 = "fdgfdsabdfsfs";
-	to_find = "ab";
+	s1 = "a";
+	to_find = "xyz";
 
-	ft_strstr(s1, to_find);
+	result = ft_strstr(s1, to_find);
+	if (result == NULL)
+		printf("NULL");
+	else
+		printf("%s\n", result);
 	return (0);
 }
