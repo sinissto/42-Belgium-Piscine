30a66098e362d60a54320b5ff1eac5ea4fa6222f
Sinisa <sinisha.stojanovic@gmail.com>
2026-07-23 01:39:56 +0200

    ex05 finished

--- 05_c_03_home/ex05/ft_strlcat.c
+++ 05_c_03_home/ex05/ft_strlcat.c
@@ -6,44 +6,34 @@
 /*   By: sstojano <sstojano@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 23:58:58 by sstojano          #+#    #+#             */
-/*   Updated: 2026/07/22 23:59:02 by sstojano         ###   ########.fr       */
+/*   Updated: 2026/07/23 01:39:14 by sstojano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-// unsigned int str_len(char *str)
-// {
-
-// }
-
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int				i;
-	int				j;
+	unsigned int	i;
+	unsigned int	j;
 	unsigned int	dest_len;
-	unsigned int	dest_len_temp;
-	unsigned int	src_len;
+	unsigned int	total_chars;
 
 	i = 0;
 	while (dest[i] != '\0')
 		i++;
-	dest_len_temp = i;
 	dest_len = i;
 	j = 0;
 	while (src[j] != '\0')
 		j++;
-	src_len = j;
-	if (dest_len >= size)
-		return (dest_len + src_len);
+	total_chars = i + j;
+	if (i >= size)
+		return (total_chars);
 	j = 0;
-	while (dest_len_temp < size - 1 && src[j] != '\0')
+	while (dest_len < size - 1 && src[j] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
+		dest[dest_len] = src[j];
 		j++;
-		dest_len_temp++;
+		dest_len++;
 	}
-	dest[i] = '\0';
-	return (dest_len + src_len);
+	dest[dest_len] = '\0';
+	return (total_chars);
 }
--- 05_c_03_home/ex05/main.c
+++ 05_c_03_home/ex05/main.c
@@ -4,12 +4,12 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 
 int	main(void)
 {
-	char			src[30] = "cdefgh";
+	char			src[30] = "x";
 	char			dest[30] = "ab";
 	unsigned int	result;
 	unsigned int	size;
 
-	size = 10;
+	size = 0;
 	printf("Dest befor: %s\n", dest);
 	result = ft_strlcat(dest, src, size);
 	printf("Result: %d\n", result);
